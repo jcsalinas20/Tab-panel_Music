67af44060d50af0b9a2105fde4d2dbd59017a3bc
jcsalinas20 <jcsalinasn20@gmail.com>
2021-03-09 17:17:14 +0100

    search artist and filter

--- www/js/init.js
+++ www/js/init.js
@@ -1,30 +1,38 @@
 (function () {
-  $(function () {
-    $('.sidenav').sidenav();
-  });
+	$(function () {
+		$('.sidenav').sidenav();
+		$("#submit").on("click", () => {
+			$.ajax({
+				url: `http://musicbrainz.org/ws/2/artist/?query=${$("#name").val()}&fmt=json`
+			}).done(function (res) {
+				$(".collection-body").html('');
+				for (const item of res.artists) {
+					$(".collection-body").append(`<a href="#!" class="collection-item">${item['name']}</a>`);
+					$(".collection-body a").last().data("body", item);
+				}
+				$(".collection-item").on("click", (e) => {
+					var tabsInstance = M.Tabs.getInstance($("#tabs"));
+					tabsInstance.select("details-tab");
+					const data = $(e.target).data("body");
+					console.log(data);
+					$(".card-panel .name").text(data.name);
+					$(".card-panel .gender").text(data.gender);
+					$(".card-panel .type").text(data.type);
+					$(".card-panel .country").text(data.country);
+					$(".card-panel .tags").html('');
+					for (const tag of data.tags)
+						$(".card-panel .tags").append(`<div class="chip">${tag.name}</div>`);
+				});
+			});
+		})
+	})
 })(jQuery);
 
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-
-  $('.tabs').tabs({
-    "swipeable": true
-  });
-
-  const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://covid-19-data.p.rapidapi.com/totals",
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "90d3685484msh1ae35a123c2c56ep17d351jsn73235bdb03b1",
-      "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
-    }
-  };
-
-  // $.ajax(settings).done(function (response) {
-  //   console.log(response);
-  // });
+	console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+	$('.tabs').tabs({
+		"swipeable": true
+	});
 }
